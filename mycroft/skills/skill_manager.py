ae6fd7b694d75cb483bf8c07505f1819b75c5086
NeonJarbas <59943014+NeonJarbas@users.noreply.github.com>
2022-06-08 22:11:43 +0100

    refactor/ready_check_bus_spam (#144)
    * refactor/ready_check_bus_spam
    
    reduce bus spam in readiness check, after a service reports ready dont query it again
    
    * add message context
    
    authored-by: jarbasai <jarbasai@mailfence.com>

--- mycroft/skills/skill_manager.py
+++ mycroft/skills/skill_manager.py
@@ -236,14 +236,18 @@ class SkillManager(Thread):
 
         services (iterable): service names to check.
         """
-        for ser in services:
-            services[ser] = False
+        for ser, rdy in services.items():
+            if rdy:
+                # already reported ready
+                continue
             if ser in ["pairing", "setup"]:
                 # pairing service (setup skill) needs to be available
                 # in offline mode (default) is_paired always returns True
                 # but setup skill may enable backend
                 # wait for backend selection event
-                response = self.bus.wait_for_response(Message('ovos.setup.state'))
+                response = self.bus.wait_for_response(Message('ovos.setup.state',
+                                                              context={"source": "skills",
+                                                                       "destination": "ovos-setup"}))
                 if response:
                     state = response.data['state']
                     LOG.debug(f"Setup state: {state}")
@@ -257,7 +261,8 @@ class SkillManager(Thread):
                 services[ser] = True
                 continue
             response = self.bus.wait_for_response(
-                Message(f'mycroft.{ser}.is_ready'))
+                Message(f'mycroft.{ser}.is_ready',
+                        context={"source": "skills", "destination": ser}))
             if response and response.data['status']:
                 services[ser] = True
         return all([services[ser] for ser in services])
