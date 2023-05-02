a51f1ab6cb98d2a3dc3a07b57a59e736be132ca6
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-05-02 16:43:44 +0100

    fix version (#317)

--- ovos_core/version.py
+++ ovos_core/version.py
@@ -15,22 +15,13 @@
 
 # The following lines are replaced during the release process.
 # START_VERSION_BLOCK
-CORE_VERSION_MAJOR = 21
-CORE_VERSION_MINOR = 2
-CORE_VERSION_BUILD = 1
-
 OVOS_VERSION_MAJOR = 0
 OVOS_VERSION_MINOR = 0
 OVOS_VERSION_BUILD = 8
-OVOS_VERSION_ALPHA = 10
+OVOS_VERSION_ALPHA = 13
 
 # END_VERSION_BLOCK
 
-
-CORE_VERSION_TUPLE = (CORE_VERSION_MAJOR,
-                      CORE_VERSION_MINOR,
-                      CORE_VERSION_BUILD)
-CORE_VERSION_STR = '.'.join(map(str, CORE_VERSION_TUPLE))
 
 OVOS_VERSION_TUPLE = (OVOS_VERSION_MAJOR,
                       OVOS_VERSION_MINOR,
@@ -41,9 +32,7 @@ OVOS_VERSION_STR = '.'.join(map(str, OVOS_VERSION_TUPLE))
 class VersionManager:
     @staticmethod
     def get():
-        return {"coreVersion": CORE_VERSION_STR,
-                "OpenVoiceOSVersion": OVOS_VERSION_STR,
-                "enclosureVersion": None}
+        return {"OpenVoiceOSVersion": OVOS_VERSION_STR}
 
 
 def check_version(version_string):
@@ -55,4 +44,4 @@ def check_version(version_string):
             version_string (string): version string ('Major.Minor.Build')
     """
     version_tuple = tuple(map(int, version_string.split('.')))
-    return CORE_VERSION_TUPLE >= version_tuple
+    return OVOS_VERSION_TUPLE >= version_tuple
