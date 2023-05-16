2ba2a185a83a0dc0038a01b6d36ffed590f33c23
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-05-16 14:49:37 -0700

    Fix `mycroft` backwards-compat. (#322)
    * Add backwards-compat import to `mycroft.client.speech.listener`
    
    * Add version params for backwards-compat
    
    * Update ovos-messagebus dependency version for compat.

--- mycroft/client/speech/listener.py
+++ mycroft/client/speech/listener.py
@@ -7,3 +7,4 @@ from ovos_listener.hotword_factory import HotWordFactory
 from ovos_listener.listener import AudioConsumer, AudioProducer, AudioStreamHandler, \
     AUDIO_DATA, STREAM_DATA, STREAM_STOP, STREAM_START, MAX_MIC_RESTARTS, \
     RecognizerLoop, RecognizerLoopState, recognizer_conf_hash
+from ovos_listener.utils import find_input_device
